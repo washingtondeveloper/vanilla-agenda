5d2cd367d49c1f1a82ec87cea22db1266ea7f39e
Washington Martins Silva <washingtonprogramador@gmail.com>
2019-02-22 13:31:24 -0300

    Update CadastroController.js

--- js/controllers/CadastroController.js
+++ js/controllers/CadastroController.js
@@ -1,58 +1,93 @@
-class CadastroService {
+'use strict'
+/**
+ * @name CadastroController
+ * @description Responsavel em fazer o Controle da Pagina de Cadastro
+ * @author Washington Developer
+ */
+class CadastroController {
 
     constructor() {
-        this.contacts = JSON.parse(localStorage.getItem('contatos')) || []
+        this.contato = {}
+        this.camposInvalidos = []
         this.id = 0
+        this.service = new CadastroService()
+
+        this.containerImage = utils.selector.query('.image')
+        this.img            = this.containerImage.children[0]
+        this.button         = utils.selector.query('#btn-cadastro')
+        this.inputId        = utils.selector.query('input[name="id"]')
+        this.inputName      = utils.selector.query('input[name="name"]')
+        this.inputEmail     = utils.selector.query('input[name="email"]')
+        this.inputPhone     = utils.selector.query('input[name="phone"]')
+        this.imageDefault   = 'img/user.png'
+
+        this.containerImage.addEventListener('click', this._clickInputFile.bind(this)) 
+        this.button.addEventListener('click', this.cadastrar.bind(this))
     }
 
-    addContato(contato) {
+    _clickInputFile(event) {
+        const file = utils.selector.id('file')
+        file.click()
 
-        if(contato.id) {
-            this.editingContato(contato)
+        file.addEventListener('change', this._changeImage.bind(this))
+    }
+
+    _changeImage(event) {
+        const file = event.target.files[0]
+
+        const that = this
+
+        const fileReader = new FileReader()
+
+        fileReader.onload = function(e) {
+            that.img.src = e.target.result
+            that.contato.img = e.target.result
+        }
+
+        fileReader.readAsDataURL(file)
+    }
+
+    cadastrar(event) {
+        event.preventDefault()
+        utils.cleanMsgErrorInput()
+
+        if(!this._validarForm()) {
+            this.camposInvalidos.forEach(c => {
+                utils.showMessage(`${c.getAttribute('placeholder')} é Obrigatório`, 'ERROR')
+                c.classList.add('input-error')
+            })
+            this.camposInvalidos = []
             return
         }
 
-        if(!contato.id) {
-            
-            contato.id = this.contacts.length + 1        
+        this.contato.id =  this.inputId.value
+        this.contato.name = this.inputName.value
+        this.contato.phone = this.inputPhone.value
+        this.contato.email = this.inputEmail.value
 
-            this.contacts.push(contato)
+        this.service.addContato(this.contato)
+        utils.showMessage('Cadastrado com Sucesso!!!', 'SUCCESS')
 
-            localStorage.removeItem('contatos')
-            localStorage.setItem('contatos', JSON.stringify(this.contacts))
-        }
-
+        this.service.cleanInputsForm([this.inputEmail, this.inputName, this.inputPhone])
+        this.img.src = this.imageDefault
+        this.contato = {}
     }
 
-    cleanInputsForm(elements) {
-        elements.forEach(element => element.value = '')
-    }
-
-    getContato(id) {
-        return this.contacts.find(c => c.id == id ? c : undefined)
-    }
-
-    editingContato(contato) {
-        let index = this.getIndexOfContato(contato)
-        console.log(index)
-        if(!contato.img)
-            contato.img = this.getContato(contato.id).img
-        
-        this.contacts.splice(index, 1, contato)
-
-        localStorage.removeItem('contatos')
-        localStorage.setItem('contatos', JSON.stringify(this.contacts))
-    }
-
-    getIndexOfContato(contato) {
-        let index = undefined
-        this.contacts.forEach((c, i) => {
-            if(c.id == contato.id) {
-                index = i
+    _validarForm() {
+        const inputs = utils.selector.all('input[placeholder]')
+        let validado = true
+        inputs.forEach(input => {
+            if(input.value.length < 3){
+                this.camposInvalidos.push(input)
+                validado = false
             }
-        })
-        return index
+                
+        } )
+        return validado
     }
 
-    
+}
+
+function cadastro() {
+    new CadastroController()
 }
